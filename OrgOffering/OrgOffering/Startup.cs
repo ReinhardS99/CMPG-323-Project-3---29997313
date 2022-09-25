2839b9609885dca8f81c18f1e5875a5c3d619957
ReinhardS99 <Steynr99@gmail.com>
2022-09-25 10:08:46 +0200

    Changed the program so yo can log in and added dependency injection

--- OrgOffering/OrgOffering/Startup.cs
+++ OrgOffering/OrgOffering/Startup.cs
@@ -27,10 +27,10 @@ namespace OrgOffering
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<CMPG323_Project3Context>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    Configuration.GetConnectionString("Persist Security Info=False;Initial Catalog=CMPG323_Project3;Data Source=REINHARD-PC\\SQLEXPRESS")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
